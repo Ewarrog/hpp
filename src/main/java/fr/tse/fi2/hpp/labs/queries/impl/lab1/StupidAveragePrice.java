f0fe0ba09c8ea7d83d1da5d85c64fc46caed31ad
Ewarrog <aurelien.touzet@orange.fr>
2015-04-20 16:29:51 +0200

    TD1 query stupid average and incremental

---
+++ src/main/java/fr/tse/fi2/hpp/labs/queries/impl/lab1/StupidAveragePrice.java
@@ -0,0 +1,33 @@
+package fr.tse.fi2.hpp.labs.queries.impl.lab1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.tse.fi2.hpp.labs.beans.DebsRecord;
+import fr.tse.fi2.hpp.labs.beans.measure.QueryProcessorMeasure;
+import fr.tse.fi2.hpp.labs.queries.AbstractQueryProcessor;
+
+public class StupidAveragePrice extends AbstractQueryProcessor {
+
+	private ArrayList<Float> liste = null;
+	
+	public StupidAveragePrice(QueryProcessorMeasure measure) {
+		super(measure);
+		// TODO Auto-generated constructor stub
+		liste = new ArrayList<Float>();
+	}
+
+	@Override
+	protected void process(DebsRecord record) {
+		// TODO Auto-generated method stub
+		
+		liste.add(record.getFare_amount());
+		
+		float sum = 0f;
+		for (Float f : liste) {
+			sum += f;
+		}
+		writeLine("current mean : " + (sum / liste.size()));;
+	}
+
+}
